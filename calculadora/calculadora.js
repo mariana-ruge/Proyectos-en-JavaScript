6211d24d32d5fe9c6682a79ade4f97c802d74213
mariana-ruge <85662130+mariana-ruge@users.noreply.github.com>
2021-12-10 22:59:18 -0500

    Create calculadora.js

---
+++ calculadora/calculadora.js
@@ -0,0 +1,53 @@
+window.addEventListener('load', ()=> 
+{
+    const display = document.querySelector('.calculator-display')
+    const botones = document.getElementsByClassName('keypad-button');
+
+    const keypadButtonsArray = Array.from(botones);
+
+    keypadButtonsArray.forEach( (button) => 
+    {
+        button.addEventListener('click', ()=> 
+        {
+            calculadora(button, display)
+        })
+    });
+});
+
+function calculadora(button, display)
+{
+    switch (button.innerHTML) 
+    {
+        case 'C':
+            
+            borrar(display);
+            break;
+        
+        case "=" :
+            calcular(display);
+            break;
+
+        default:
+            actualizar(display, button);
+            break;
+    }
+}
+
+function calcular(display)
+{
+    display.innerHTML = eval(display.innerHTML)
+}
+
+function actualizar(display, button)
+{
+    if(display.innerHTML== 0)
+    {
+        display.innerHTML = '';
+    }
+    display.innerHTML += button.innerHTML;
+}
+
+function borrar(display)
+{
+    display.innerHTML= 0;
+}
